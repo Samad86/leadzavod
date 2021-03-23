013f2498bafdf158195773629c3d31323ac4d364
samad86 <mads198644@gmail.com>
2021-03-23 23:52:40 +0300

    add leadzavod

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.content__slider').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrows: false,
+  fade: true,
+  cssEase: 'linear',
+  easing: 'linear',
+  waitForAnimate: 'true',
+});
